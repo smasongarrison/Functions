d877a313dd45bdaca9e87b43ae953b698b72893e
Mason Garrison <sarah.m.garrison@vanderbilt.edu>
2015-08-20 17:10:25 -0700

    added lm extract

--- Mason_Functions.R
+++ Mason_Functions.R
@@ -128,3 +128,12 @@ Sd_1 <- function(x) round(stats::sd(x, na.rm=TRUE),1)
 Sd_2 <- function(x) round(stats::sd(x, na.rm=TRUE),2)
 Sd_3 <- function(x) round(stats::sd(x, na.rm=TRUE),3)
 Sd_4 <- function(x) round(stats::sd(x, na.rm=TRUE),4)
+
+##
+lm_eqn = function(df){
+  m = lm(y ~ x, df);
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, list(a = format(coef(m)[1], digits = 2), 
+                                                                                  b = format(coef(m)[2], digits = 2), 
+                                                                                  r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));                 
+}
